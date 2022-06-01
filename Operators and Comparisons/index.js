d098b4bfa96d450a46389566f823ef1151e46825
Shubham <shubhsahab@gmail.com>
2022-06-01 15:21:40 +0530

    All about Operators and Comparison

---
+++ Operators and Comparisons/index.js
@@ -0,0 +1,48 @@
+//----------------Addition----------------
+console.log(2 + 3); //5
+
+//----------------Subtraction----------------
+console.log(6 - 3); //3
+console.log(3 - 6); //-3
+
+//----------------Multiplication----------------
+console.log(5 * 4); //20
+
+//----------------Division----------------
+console.log(5 / 4); //1.25
+
+//----------------Remainder----------------
+console.log(13 % 3); //1
+
+//----------------Exponential----------------
+console.log(3 ** 5); //243
+
+//----------------String Addition----------------
+console.log("Appventurez" + " " + "Mobitech"); //Appenturez Mobitech
+
+//----------------Unary +----------------
+
+// + doesn't change the number
+console.log(+1); //1
+
+// + convert into number like Number()
+console.log(+true); //1
+
+//----------------Increment/decrement----------------
+let a = 3;
+console.log(++a); //4
+console.log(a--); //4
+
+//----------------Comparison----------------
+
+//Number
+console.log(4 > 5); //false
+console.log(4 < 5); //true
+console.log(!5); //false
+console.log(!0); //true
+
+//String
+/* strings are compared by letter by letter*/
+console.log('Z' > 'A'); //true
+console.log('Swiggy' > 'Swing'); //false
+console.log('2' > '1'); //true
