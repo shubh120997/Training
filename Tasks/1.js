c251d9b5041f260bfd9545e96c29d8ddf135c3e1
Shubham <shubhsahab@gmail.com>
2022-06-01 17:53:42 +0530

    Add the problem part

--- Tasks/1.js
+++ Tasks/1.js
@@ -1,3 +1,18 @@
+/* Task -- Given an array of integers, keep a total score based on the following:
+1. Add 1 point for every even number in the array
+2. Add 3 points for every odd number in the array, except for the number "5"
+3. Add 5 points every time the number "5" appears in the array
+4. Add 1 point for every 0 in  the array
+
+Input: [1, 2, 3, 4, 5]  Output: 13
+Input: [17, 19, 21] Output: 9
+Input: [5, 5, 5] Output: 15 */
+
+
+
+/*-------------------Coding Part-------------------*/
+
+
 // const arr1 = [1, 2, 3, 4, 5];
 // const arr1 = [17, 19, 21];
 const arr1 = [5, 5, 5];
